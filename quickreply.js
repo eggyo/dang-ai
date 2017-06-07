714488656a7c7fc25a79600b1a7318538d58fa9a
eggyo <eggyoyo4859@gmail.com>
2017-06-07 09:52:04 +0700

    v.1

--- quickreply.js
+++ quickreply.js
@@ -1,5 +1,3 @@
-_metadata = require('./metadata.js');
-
 module.exports = {
   payloadProcess: function(recipientId, payload) {
     try {
@@ -49,7 +47,7 @@ module.exports = {
             }
           }
           if (newNextQuizs.length != 0) {
-            _metadata.parseQuizObjectToMessage(currentQuiz, function(response) {
+            parseQuizObjectToMessage(currentQuiz, function(response) {
 
               if (response != null) {
                 var quiz = response.quiz;
@@ -85,12 +83,12 @@ module.exports = {
                 console.log("messageData " + JSON.stringify(messageData) + " quick_replies:" + JSON.stringify(quick_replies));
 
                 responseData({
-                  "results": [messageText,messageData]
+                  "results": [messageText, messageData]
                 });
               }
             });
           } else {
-            _metadata.parseQuizObjectToMessage(currentQuiz, function(response) {
+            parseQuizObjectToMessage(currentQuiz, function(response) {
 
               if (response != null) {
                 var quiz = response.quiz;
@@ -125,7 +123,7 @@ module.exports = {
                 console.log("messageData " + JSON.stringify(messageData) + " quick_replies:" + JSON.stringify(quick_replies));
 
                 responseData({
-                  "results": [messageText,messageData]
+                  "results": [messageText, messageData]
                 });
               }
             });
@@ -148,3 +146,69 @@ module.exports = {
 
   }
 };
+
+
+
+
+function getParseQuizObject(objectId, responseMsg) {
+  var options = {
+    url: 'https://eggyo-quiz-db.herokuapp.com/parse/classes/Quiz/' + objectId,
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Parse-Application-Id': 'myAppId',
+      'X-Parse-REST-API-Key': 'myRestKey'
+    }
+  };
+
+  function callback(error, response, body) {
+    console.log("response:" + JSON.stringify(response));
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      responseMsg(info);
+    } else {
+      console.error("Unable to send message. Error :" + error);
+    }
+  }
+  request(options, callback);
+}
+
+
+
+
+function parseQuizObjectToMessage(objectId, quizMsg) {
+  getParseQuizObject(objectId, function(response) {
+    if (response != null) {
+      var quiz = response.quiz;
+      var correct_ans = response.correct_ans;
+      var incorrect_ans = response.incorrect_ans;
+      var correct_index = Math.floor(Math.random() * 4) + 1;
+      var messageQuiz = quiz;
+      var incorrect_index = 1;
+      for (var i = 0; i < incorrect_ans.length; i++) {
+        var inc = incorrect_ans[i];
+        if ((i + 1) == correct_index) {
+          messageQuiz += "\n" + (correct_index) + ". " + correct_ans;
+          incorrect_index += 1;
+          messageQuiz += "\n" + (incorrect_index) + ". " + inc;
+          incorrect_index += 1;
+
+        } else {
+          messageQuiz += "\n" + (incorrect_index) + ". " + inc;
+          incorrect_index += 1;
+          if (incorrect_index == correct_index) {
+            messageQuiz += "\n" + (correct_index) + ". " + correct_ans;
+
+          }
+        }
+      }
+      quizMsg({
+        quiz: messageQuiz,
+        correct_index: correct_index,
+        choice_count: incorrect_ans.length + 1
+      });
+    } else {
+      quizMsg({});
+    }
+  });
+}
